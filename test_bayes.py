26b260da3caa4c9725bc26a8fc2f6ac72eeef8ec
Zachary Gallegos <zpgallegos@gmail.com>
2017-05-06 17:48:14 -0700

    working

--- test_bayes.py
+++ test_bayes.py
@@ -1,5 +1,5 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 import os
 import re
 
@@ -8,6 +8,7 @@ from sklearn.base import clone
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import log_loss, accuracy_score
 from sklearn.datasets import load_svmlight_file
+from sklearn.linear_model import SGDClassifier
 
 
 def create_ensemble(classifier, X, num, num_features):
@@ -45,6 +46,25 @@ def fit_ensemble(ensemble, x, y, partial = False, classes = None):
             e.partial_fit(x_, y, classes = classes)
 
     return ensemble
+
+
+def fit_sgd_ensemble(ensemble, X, y):
+    sgd = SGDClassifier(loss='log')
+    ensemble_prediction = []
+    for e in ensemble:
+
+        X_ = X[:, e.sub_feats]
+
+        e.fit(X_, y)
+        ensemble_prediction.append(e.predict_proba(X_)[:,1])
+    return sgd.fit(np.array(ensemble_prediction).transpose(),y)
+
+
+def predict_sgd(ensemble, fitted_sgd, X):
+    """
+    """
+    ensemble_preds = [e.predict_proba(X[:, e.sub_feats])[:,1][0] for e in ensemble]
+    return fitted_sgd.predict(np.array(ensemble_preds).reshape(1,-1))
 
 
 def update_bayes(ensemble, i, x, y, classes, a, b, theta):
@@ -107,7 +127,31 @@ def predict_vote(ensemble, X, classes):
     return classes[np.argmax(pred)]
 
 
-def online_test(ensemble, single, X, y, classes, start_prop, **kwargs):
+def output_loss(ensemble, dataset, trial):
+    """
+    """
+    end_lams = np.array([e.lam[-1] for e in ensemble])
+
+    min_ind = np.argmin(end_lams)
+    max_ind = np.argmax(end_lams)
+    med_ind = np.argsort(end_lams)[len(end_lams) // 2]
+
+    lams_towrite = np.stack((ensemble[min_ind].lam,
+                             ensemble[med_ind].lam,
+                             ensemble[max_ind].lam), axis = 1)
+
+    loss_towrite = np.stack((ensemble[min_ind].loss,
+                             ensemble[med_ind].loss,
+                             ensemble[max_ind].loss), axis = 1)
+
+    df = pd.DataFrame(lams_towrite, columns = ['lam_min', 'lam_med', 'lam_max'])
+    df.to_csv(os.path.join('results', 'loss', dataset + '_lam_samp_' + str(trial) + '.csv'))
+
+    df = pd.DataFrame(loss_towrite, columns = ['loss_min', 'loss_med', 'loss_max'])
+    df.to_csv(os.path.join('results', 'loss', dataset + '_loss_samp_' + str(trial) + '.csv'))
+
+
+def online_test(ensemble, single, X, y, classes, start_prop, do_sgd, **kwargs):
     """
     perform the tests
     """
@@ -119,6 +163,7 @@ def online_test(ensemble, single, X, y, classes, start_prop, **kwargs):
     single_preds = [None] * stream_size
     vote_preds = [None] * stream_size
     bayes_preds = [None] * stream_size
+    sgd_preds = [None] * stream_size
 
     for e in ensemble:
 
@@ -132,6 +177,13 @@ def online_test(ensemble, single, X, y, classes, start_prop, **kwargs):
 
     single.fit(X_start, y_start)
     ensemble = fit_ensemble(ensemble, X_start, y_start)
+
+    if do_sgd:
+
+        fitted_sgd = (fit_sgd_ensemble(ensemble, X_start, y_start))
+
+    _X_ = X_start
+    _y_ = y_start
 
     for j in range(stream_size):
 
@@ -152,6 +204,10 @@ def online_test(ensemble, single, X, y, classes, start_prop, **kwargs):
         vote_preds[j] = predict_vote(ensemble, x_, classes)
         bayes_preds[j] = predict_bayes(ensemble, j - 1, x_, classes)
 
+        if do_sgd:
+
+            sgd_preds[j] = predict_sgd(ensemble, fitted_sgd, x_)[0]
+
         """
         partial fit
         """
@@ -159,11 +215,21 @@ def online_test(ensemble, single, X, y, classes, start_prop, **kwargs):
         single.partial_fit(x_, y_, classes = classes)
         ensemble = fit_ensemble(ensemble, x_, y_, partial = True, classes = classes)
 
+        _X_=(np.vstack((_X_,x_)))
+        _y_=(np.append(_y_,y_))
+
+        if do_sgd:
+
+            fitted_sgd = fit_sgd_ensemble(ensemble, _X_, _y_)
+
     single_preds = np.array(single_preds).reshape(y_stream.shape)
     vote_preds = np.array(vote_preds).reshape(y_stream.shape)
     bayes_preds = np.array(bayes_preds).reshape(y_stream.shape)
+    sgd_preds = np.array(sgd_preds).reshape(y_stream.shape)
 
-    return single_preds, vote_preds, bayes_preds, y_stream
+    return (ensemble, single_preds, vote_preds, 
+            bayes_preds, sgd_preds, y_stream)
+
 
 
 
@@ -175,14 +241,25 @@ if __name__ == '__main__':
 
     os.chdir('/users/zgallegos/documents/school/math_538/project/data')
 
-    use_datasets = ['heart.txt', 'mushrooms.txt', 'australian.txt',
-                    'ionosphere.txt', 'sonar.txt']
-    use_classes = [(-1, 1), (1, 2), (-1, 1), (-1, 1), (-1, 1)]
+    use_datasets = ['heart.txt', 'australian.txt', 'ionosphere.txt', 
+                    'sonar.txt', 'mushrooms.txt']
+    use_classes = [(-1, 1), (-1, 1), (-1, 1), (-1, 1), (1, 2)]
+
+    use_datasets = ['mushrooms.txt']
+    use_classes = [(1, 2)]
 
     n_trials = 10
     feat_perc = .5 # percentage of the features ensemble classifiers get
 
     for j, k in enumerate(use_datasets):
+
+        if k == 'mushrooms.txt':
+
+            do_sgd = False
+
+        else:
+
+            do_sgd = True
         
         fl_name = re.search('^.+?(?=\.txt)', k).group(0)
         err_fl = fl_name + '_errors.csv'
@@ -198,6 +275,7 @@ if __name__ == '__main__':
         sing = []
         vote = []
         bayes = []
+        sgd = []
 
         trlz = range(n_trials)
 
@@ -208,27 +286,32 @@ if __name__ == '__main__':
             single = naive_bayes.BernoulliNB()
             ensemble = create_ensemble(naive_bayes.BernoulliNB(), X, 100, feats)
 
-            s, v, b, y_ = online_test(ensemble, single, X, y, cls, 
-                            start_prop = .1, a = 1, b = 1, theta = .1)
+            e, s, v, b, d, y_ = online_test(ensemble, single, X, y, cls, .1, do_sgd, 
+                                                a = 1, b = 1, theta = .1)
 
             indx = range(1, len(y_) + 1)
             cum_sing = 1 - np.cumsum(s == y_) / indx
             cum_vote = 1 - np.cumsum(v == y_) / indx
             cum_bayes = 1- np.cumsum(b == y_) / indx
+            cum_sgd = 1- np.cumsum(d == y_) / indx
 
-            to_write = np.stack((indx, cum_sing, cum_vote, cum_bayes), axis = 1)
+            to_write = np.stack((indx, cum_sing, cum_vote, cum_bayes, cum_sgd), axis = 1)
             
-            df = pd.DataFrame(to_write, columns = ['index', 'single', 'vote', 'bayes'])
+            df = pd.DataFrame(to_write, columns = ['index', 'single', 'vote', 'bayes', 'sgd'])
             df.to_csv(os.path.join('results', 'cumulative', cumerr_fl))
 
             sing.append(1 - accuracy_score(y_, s))
             vote.append(1 - accuracy_score(y_, v))
             bayes.append(1 - accuracy_score(y_, b))
+            sgd.append(1 - accuracy_score(y_, d))
 
-        to_write = np.stack((trlz, sing, vote, bayes), axis = 1)
+            output_loss(e, fl_name, i)
 
-        df = pd.DataFrame(to_write, columns = ['trial', 'single', 'vote', 'bayes'])
+        to_write = np.stack((trlz, sing, vote, bayes, sgd), axis = 1)
+
+        df = pd.DataFrame(to_write, columns = ['trial', 'single', 'vote', 'bayes', 'sgd'])
         df.to_csv(os.path.join('results', 'accuracy', err_fl))
+
 
 
 
